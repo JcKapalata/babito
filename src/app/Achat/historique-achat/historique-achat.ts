2f90365ff0be270caaa0f88d203f06b059bd70c1
JcKaps <kapalatajc@gmail.com>
2025-12-23 01:58:16 +0200

    amelioration du historique achat en le trie de status

--- src/app/Achat/historique-achat/historique-achat.ts
+++ src/app/Achat/historique-achat/historique-achat.ts
@@ -19,19 +19,30 @@ export class HistoriqueAchat {
   // Etat (Signals)
   commandes = signal<ProduitAchete[]>([]);
   chargement = signal<boolean>(false);
+  filtreStatut = signal<string>('tous');
 
-  // Création du signal trié
-  commandesTriees = computed(() => {
-    return [...this.commandes()].sort((a, b) => {
-      // On transforme les dates en timestamp pour la comparaison
-      const dateA = new Date(a.dateAchat).getTime();
-      const dateB = new Date(b.dateAchat).getTime();
-      return dateB - dateA; // Ordre décroissant (plus récent en premier)
+  // Signal intelligent qui trie par date ET filtre par statut
+  commandesTrieesEtFiltrees = computed(() => {
+    let liste = [...this.commandes()];
+
+    // 1. Filtrage
+    if (this.filtreStatut() !== 'tous') {
+      liste = liste.filter(a => a.status.toLowerCase() === this.filtreStatut().toLowerCase());
+    }
+
+    // 2. Tri par date (le plus récent en premier)
+    return liste.sort((a, b) => {
+      return new Date(b.dateAchat).getTime() - new Date(a.dateAchat).getTime();
     });
   });
 
   ngOnInit(): void {
     this.chargerDonnees();
+  }
+
+  // Méthode pour changer le filtre
+  changerFiltre(nouveauStatut: string) {
+    this.filtreStatut.set(nouveauStatut);
   }
 
   private chargerDonnees(): void {
